e90488411648e6bbb903ba9b9fc52e00fa98e5c2
yiyun <yiyungent@126.com>
2020-08-10 19:34:01 +0800

    feat: aduSkin demo

--- src/Persentation/WebInstaller/MainWindow.xaml.cs
+++ src/Persentation/WebInstaller/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AduSkin.Controls.Metro;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,7 +19,7 @@ namespace WebInstaller
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
